0f5d59ea1655c2661c4fef618ada1e33343196c8
Daniel_LPT <rhdperez@hotmail.com>
2021-07-19 12:48:24 -0300

    comenzandoBkd

---
+++ FBTarjeta/AplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FBTarjeta.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*Clase que se usa para migrar datos al modelo.
+ Crea una instancia de nuestra BD para que podamos editarla y crear consultas*/
+
+namespace FBTarjeta
+{
+    public class AplicationDbContext: DbContext
+    {
+
+        DbSet<TarjetaCredito> TarjetaCredito { set; get; } //aca se mapea nuestro modelo con la BD
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base (options)
+        {
+
+        }
+    }
+}
